9e9690f5b8c7333152b7203029a89fc40e3dbe7d
glennludszuweit <glenn.ludszuweit@gmail.com>
2023-10-20 13:34:23 +0100

    companionai initial message

--- components/ChatMessages.tsx
+++ components/ChatMessages.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ElementRef, useEffect, useRef } from 'react';
+import { ElementRef, useEffect, useRef, useState } from 'react';
 import { Companion } from '@prisma/client';
 import ChatMessage, { ChatMessageProps } from '@/components/ChatMessage';
 
@@ -16,6 +16,19 @@ const ChatMessages = ({
   isLoading,
 }: ChatMessagesProps) => {
   const scrollRef = useRef<ElementRef<'div'>>(null);
+  const [fakeIsLoading, setFakeIsLoading] = useState<boolean>(
+    messages.length === 0 ? true : false
+  );
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setFakeIsLoading(false);
+    }, 2500);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
 
   useEffect(() => {
     scrollRef?.current?.scrollIntoView({ behavior: 'smooth' });
@@ -23,6 +36,13 @@ const ChatMessages = ({
 
   return (
     <div className='flex-1 overflow-y-auto pr-1'>
+      <ChatMessage
+        isLoading={fakeIsLoading}
+        role='system'
+        content={`Hello! My name is ${companion.name} – ${companion.description}`}
+        imageUrl={companion.imageUrl}
+      />
+
       {messages.map((message) => (
         <ChatMessage
           key={message.content}
@@ -36,7 +56,7 @@ const ChatMessages = ({
         <ChatMessage role='system' imageUrl={companion.imageUrl} isLoading />
       )}
 
-      <div ref={scrollRef}></div>
+      <div ref={scrollRef} />
     </div>
   );
 };
